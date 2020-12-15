00c4394e3be2b817d035347cc657703954625917
Robert Horngren <robert@horngren.net>
2020-12-15 14:37:18 +0100

    basic ui

--- swift-tac-toe/ViewController.swift
+++ swift-tac-toe/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func action(_ sender: Any) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
