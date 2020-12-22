2418721343535b1abfda433276a0ede938f26f17
Robert Horngren <robert@horngren.net>
2020-12-22 11:35:26 +0100

    minor code cleanup, added board grid

--- swift-tac-toe/ViewController.swift
+++ swift-tac-toe/ViewController.swift
@@ -12,29 +12,39 @@ class ViewController: UIViewController {
     var activePlayer = 1
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     
-    var winningCombination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    var winningCombinationSmall = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     
     var gameIsActive = true
     
     var gameTurn = 0
     var winner = false
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        playAgainButton.isHidden = true
+        winningPlayer.isHidden = true
+
+    }
+    
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var winningPlayer: UILabel!
+    
     @IBAction func action(_ sender: Any) {
         if gameState[(sender as AnyObject).tag-1] == 0 && gameIsActive == true {
             gameState[(sender as AnyObject).tag-1] = activePlayer
             gameTurn += 1
             if activePlayer == 1 {
-//                gameTurn += 1
                 (sender as AnyObject).setImage(UIImage(named: "Cross.png"), for: UIControl.State())
                 activePlayer = 2
+                
             } else {
-//                gameTurn += 1
                 (sender as AnyObject).setImage(UIImage(named: "Nought.png"), for: UIControl.State())
                 activePlayer = 1
             }
         }
         
-        for combination in winningCombination {
+        for combination in winningCombinationSmall {
             if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
                 gameIsActive = false
                 
@@ -51,9 +61,7 @@ class ViewController: UIViewController {
                 winningPlayer.isHidden = false
             }
         }
-        
         gameIsActive = false
-        
         
         for i in gameState {
             if i == 0 {
@@ -63,25 +71,12 @@ class ViewController: UIViewController {
         }
         
         while (gameTurn == 9) && (winner == false) {
+            //setting gameTurn to 255 to break while-loop
             gameTurn = 255
-            
             winningPlayer.text = "Draw!"
             winningPlayer.isHidden = false
             playAgainButton.isHidden = false
         }
-        
-    }
-    
-    
-    @IBOutlet weak var playAgainButton: UIButton!
-    @IBOutlet weak var winningPlayer: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        playAgainButton.isHidden = true
-        winningPlayer.isHidden = true
-
     }
 
     @IBAction func playAgain(_ sender: Any) {
